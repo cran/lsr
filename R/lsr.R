68cd8aae3da6fc7cb74b131822cafaff8de4da67
Daniel Navarro <daniel.navarro@adelaide.edu.au>
2013-04-19 00:00:00 +0000

    version 0.2.3

--- R/lsr.R
+++ R/lsr.R
@@ -301,19 +301,24 @@ cramersV <- function (...) {
   test <- chisq.test(...)
   chi2 <- test$statistic
   N <- sum(test$observed)
-  
-  # for GOF test, give same answer as phi
+ 
   if (test$method =="Chi-squared test for given probabilities"){
-    k <- 2
+    # for GOF test, calculate max chi-square value 
+    ind <- which.min(test$expected)
+    max.dev <- test$expected
+    max.dev[ind] <- N-max.dev[ind]
+    max.chi2 <- sum( max.dev ^2 / test$expected ) 
+    V <- sqrt( chi2 / max.chi2 )
   } 
-  else { # otherwise, correct for size of table
-    S <- dim(test$observed)
-    k <- min(S)
+  else { 
+    # for test of association, use analytic expression
+    k <- min(dim(test$observed))
+    V <- sqrt( chi2 / (N*(k-1)) )
   }
-  
-  V <- sqrt( chi2 / (N*(k-1)) )
   names(V) <- NULL
-  return(V)
+  return(V)  
+  
+
   
 }
 
